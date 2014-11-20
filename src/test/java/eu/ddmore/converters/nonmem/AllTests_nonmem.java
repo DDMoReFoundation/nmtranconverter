59957763266a2841d74d8b87ba68420fba9cc72d
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2014-11-20 14:27:34 +0000

    Changed package structure to eu.ddmore from ddmore and also added services to src/main/resources

---
+++ src/test/java/eu/ddmore/converters/nonmem/AllTests_nonmem.java
@@ -0,0 +1,33 @@
+
+
+package eu.ddmore.converters.nonmem;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestExample5.class
+	//TestSine_Wave2.class,
+	//TestSine_Wave3.class,
+	//TestSine_Wave4.class,
+	//TestSine_Wave5.class,
+	//TestSine_Wave6.class,
+	//TestTimed_Linked.class,
+	//TestConstant_Funcs.class,
+	//TestDamped_SHM.class,
+	//TestLogx_Funcs.class,
+	//TestRigidBody1.class,
+	//TestRigidBody2.class,
+	//TestRoot_Funcs.class,
+	//Test_Error_Model1.class,
+	//Test_Continuous_Univariate_Distributions.class,
+	//Test_Mixed_Effect_Simulation_PK_a1.class,
+	//Test_Mixed_Effect_Simulation_PK_a2.class,
+	//Test_Continous_PK_with_Bolus_Dosing1.class,
+	//Test_Mixed_Effect_Simulation_No_Dosing.class,
+	//Test_Mixed_Effect_Simulation_No_Dosing_With_Error_Model.class
+})
+public class AllTests_nonmem {
+
+}
