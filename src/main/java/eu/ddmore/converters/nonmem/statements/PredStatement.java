84be474a583d2b067fa3eb17df44a739a45ca9bd
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2014-11-25 12:28:40 +0000

    Added changes for Omega blocks

--- src/main/java/eu/ddmore/converters/nonmem/statements/PredStatement.java
+++ src/main/java/eu/ddmore/converters/nonmem/statements/PredStatement.java
@@ -20,8 +20,8 @@ import eu.ddmore.libpharmml.dom.commontypes.RealValueType;
 import eu.ddmore.libpharmml.dom.commontypes.VariableDefinitionType;
 import eu.ddmore.libpharmml.dom.maths.FunctionCallType;
 import eu.ddmore.libpharmml.dom.modeldefn.GaussianObsError;
-import eu.ddmore.libpharmml.dom.modeldefn.GaussianObsError.ErrorModel;
 import eu.ddmore.libpharmml.dom.modeldefn.IndividualParameterType;
+import eu.ddmore.libpharmml.dom.modeldefn.GaussianObsError.ErrorModel;
 
 /**
  * Creates and adds estimation statement to nonmem file from script definition.
@@ -90,11 +90,7 @@ public class PredStatement {
 		List<ParameterBlock> blocks = scriptDefinition.getParameterBlocks();
 		predCoreBlock.append(buildThetaAssignments().toString());
 		
-		for(ParameterBlock parameterBlock : blocks){
-			for(IndividualParameterType parameterType: parameterBlock.getIndividualParameters()){
-				predCoreBlock.append(doIndividualParameterAssignment(parameterType));	
-			}
-		}
+		predCoreBlock.append(getAllIndividualParamAssignments(blocks));
 		return predCoreBlock;
 	}
 
@@ -193,9 +189,9 @@ public class PredStatement {
 	 * gets ABBR block for pred statement of nonmem file.
 	 * TODO Currently ABBR block is not in scope.
 	 */
-	private StringBuilder getAbbreviatedStatement() {
-		return null;
-	}
+//	private StringBuilder getAbbreviatedStatement() {
+//		return null;
+//	}
 
 	/**
 	 * get model statement block for pred statement of nonmem file.
@@ -257,12 +253,18 @@ public class PredStatement {
 	}
 	
 	/**
-	 * This method will create part of pred core statement block from individual parameter assignments.
-	 * Currently it refers to  
-	 * @param ip
+	 * This method will collect all the parsing for Individual parameter assignments.
+	 *  
+	 * @param blocks
 	 * @return
 	 */
-	public String doIndividualParameterAssignment(IndividualParameterType ip) {
-		return parser.doIndividualParameterAssignment(ip);
+	public StringBuilder getAllIndividualParamAssignments(List<ParameterBlock> blocks) {
+		StringBuilder IndividualParamAssignmentBlock = new StringBuilder();
+		for(ParameterBlock parameterBlock : blocks){
+			for(IndividualParameterType parameterType: parameterBlock.getIndividualParameters()){
+				IndividualParamAssignmentBlock.append(parser.doIndividualParameterAssignment(parameterType));	
+			}
+		}
+		return IndividualParamAssignmentBlock;
 	}
 }
--- src/main/java/eu/ddmore/converters/nonmem/utils/ParametersHelper.java
+++ src/main/java/eu/ddmore/converters/nonmem/utils/ParametersHelper.java
@@ -2,14 +2,16 @@ package eu.ddmore.converters.nonmem.utils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import crx.converter.engine.ScriptDefinition;
 import crx.converter.engine.parts.BaseRandomVariableBlock.Correlation;
 import crx.converter.engine.parts.EstimationStep;
 import crx.converter.engine.parts.EstimationStep.FixedParameter;
-import crx.converter.engine.parts.ObservationBlock;
 import crx.converter.engine.parts.ParameterBlock;
 import crx.converter.engine.parts.Part;
 import eu.ddmore.converters.nonmem.statements.OmegaStatement;
@@ -27,11 +29,19 @@ public class ParametersHelper {
 	
 	ScriptDefinition scriptDefinition;
 	List<SimpleParameterType> simpleParameterTypes = new ArrayList<SimpleParameterType>();
-	final Map<String, ThetaStatement> thetaStatements= new HashMap<String, ThetaStatement>();
-	final Map<String, OmegaStatement> OmegaStatements= new HashMap<String, OmegaStatement>();
+	final Map<String, ThetaStatement> thetaStatements = new HashMap<String, ThetaStatement>();
+	final Map<String, OmegaStatement> OmegaStatements = new HashMap<String, OmegaStatement>();
+	
+	final Map<String, List<OmegaStatement>> OmegaBlocks = new HashMap<String, List<OmegaStatement>>();
+	String omegaBlockTitle;
+
+//	Map<String, String> randomVarsInCorrelation = new HashMap<String>();
+	Map<String, String> etasToOmegasInCorrelation = new HashMap<String, String>();
 	
 	// These are keyed by symbol ID
 	final Map<String, SimpleParameterType> simpleParams = new HashMap<String, SimpleParameterType>();
+	Map<String, Integer> etaToOmagaMap = new HashMap<String, Integer>();
+	Map<Integer, String> orderedEtasMap;
 	List<ParameterEstimateType> parametersToEstimate = new ArrayList<ParameterEstimateType>();
 	List<FixedParameter> fixedParameters = new ArrayList<FixedParameter>();
 	final Map<String, InitialEstimateType> initialEstimates = new HashMap<String, InitialEstimateType>();
@@ -54,6 +64,8 @@ public class ParametersHelper {
 		setAllParameterBounds(parametersToEstimate);
 		
 		//need to set omegas before setting theta params
+		//setOmegaBlocks before omega params and theta
+		createOmegaBlocks();
 		setOmegaParameters();
 		setThetaParameters();
 		
@@ -71,64 +83,210 @@ public class ParametersHelper {
 
 		for(ParameterEstimateType parameter : parametersToEstimate){
 			String paramName = parameter.getSymbRef().getSymbIdRef();
-				if(!(OmegaStatements.containsKey(paramName) || paramName.startsWith("sigma") || thetaStatements.containsKey(paramName))){
-					if(paramName!= null){
-						ThetaStatement thetaStatement = new ThetaStatement(paramName);
-						thetaStatement.setParameterBounds(initialEstimates.get(paramName),lowerBounds.get(paramName),upperBounds.get(paramName));
-						thetaStatements.put(paramName, thetaStatement);					
+			if(validateParamName(paramName)){
+				ThetaStatement thetaStatement = new ThetaStatement(paramName);
+				thetaStatement.setParameterBounds(initialEstimates.get(paramName),lowerBounds.get(paramName),upperBounds.get(paramName));
+				thetaStatements.put(paramName, thetaStatement);					
+			}
+		}
+		for(FixedParameter fixedParameter : fixedParameters){
+			String paramName = fixedParameter.p.getSymbRef().getSymbIdRef();
+			if(validateParamName(paramName)){
+				ThetaStatement thetaStatement = new ThetaStatement(paramName);
+				thetaStatement.setParameterBounds(initialEstimates.get(paramName),lowerBounds.get(paramName),upperBounds.get(paramName));
+				thetaStatement.setFixed(true);
+				thetaStatements.put(paramName, thetaStatement);					
+			}
+		}
+	}
+
+	private boolean validateParamName(String paramName) {
+		return !(paramName== null ||  OmegaStatements.containsKey(paramName) || etasToOmegasInCorrelation.values().contains(paramName) || 
+				paramName.startsWith("sigma") || thetaStatements.containsKey(paramName));
+	}
+	
+	/**
+	 * This method will create omega blocks if there are any.
+	 * We will need ordered etas and eta to omega map to determine order of the omega block elements.
+	 * Currently only correlations are supported.
+	 *  
+	 * @return
+	 */
+	public void createOmegaBlocks(){
+		List<Correlation> correlations = getAllCorrelations();
+		
+		if(!correlations.isEmpty()){
+			initialiseOmegaBlocks(correlations);
+			orderedEtasMap = reverseMap(etaToOmagaMap);
+			omegaBlockTitle = createOmegaBlockTitle(correlations);
+
+			for(String eta : orderedEtasMap.values()){
+
+				for(Correlation correlation :  correlations){
+					String randomVar1 = correlation.rnd1.getSymbId();
+					String randomVar2 = correlation.rnd2.getSymbId();
+					int column = getOrderedEtaIndex(randomVar1);
+					int row = getOrderedEtaIndex(randomVar2);
+
+					createFirstMatrixRow(eta, correlation.rnd1);
+						
+					List<OmegaStatement> omegas = OmegaBlocks.get(randomVar2);
+					if(omegas.get(row)==null){
+						omegas.remove(row);
+						String symbId = getNameFromParamRandomVariable(correlation.rnd2);
+						omegas.add(row, getOmegaFromRandomVariable(symbId));
+					}
+					
+					if(omegas.get(column)==null){
+						String symbId = correlation.correlationCoefficient.getSymbRef().getSymbIdRef();
+						omegas.remove(column);
+						omegas.add(column, getOmegaFromRandomVariable(symbId));	
 					}
 				}
 			}
-		for(FixedParameter fixedParameter : fixedParameters){
-			String paramName = fixedParameter.p.getSymbRef().getSymbIdRef();
-			if(!(OmegaStatements.containsKey(paramName) || paramName.startsWith("sigma") || thetaStatements.containsKey(paramName))){
-					if(paramName!= null){
-						ThetaStatement thetaStatement = new ThetaStatement(paramName);
-						thetaStatement.setParameterBounds(initialEstimates.get(paramName),lowerBounds.get(paramName),upperBounds.get(paramName));
-						thetaStatement.setFixed(true);
-						thetaStatements.put(paramName, thetaStatement);					
-					}
-					
-				}
+		}
+	}
+
+	/**
+	 * Creates title for omega blocks.
+	 * Currently it gets block count for BLOCK(n) and identify in correlations are used.
+	 * This will be updated for matrix types in future.
+	 * 
+	 * @param correlations
+	 * @return
+	 */
+	private String createOmegaBlockTitle(List<Correlation> correlations) {
+		Integer blocksCount = etaToOmagaMap.size();
+		//This will change in case of 0.4 as it will need to deal with matrix types as well.
+		String description = (!correlations.isEmpty())?"CORRELATION":"";
+		String title = "\n$OMEGA BLOCK("+blocksCount+") "+description;
+		return title;
+	}
+
+	/**
+	 * This method will return index from ordered eta map for random var name provided. 
+	 * 
+	 * @param randomVariable
+	 * @return
+	 */
+	private int getOrderedEtaIndex(String randomVariable) {
+		return (etaToOmagaMap.get(randomVariable)>0)?etaToOmagaMap.get(randomVariable)-1:0;
+	}
+	
+	/**
+	 * This method will reverse the map and return a tree map (ordered in natural order of keys).
+	 * 
+	 * @param map
+	 * @return
+	 */
+	private <K,V> TreeMap<V,K> reverseMap(Map<K,V> map) {
+		TreeMap<V,K> rev = new TreeMap<V, K>();
+	    for(Map.Entry<K,V> entry : map.entrySet())
+	        rev.put(entry.getValue(), entry.getKey());
+	    return rev;
+	}
+
+	/**
+	 * Collects correlations from all the prameter blocks. 
+	 * 
+	 * @return
+	 */
+	private List<Correlation> getAllCorrelations() {
+		List<Correlation> correlations = new ArrayList<Correlation>();
+		List<ParameterBlock> parameterBlocks = getScriptDefinition().getParameterBlocks();
+		if(!parameterBlocks.isEmpty()){
+			for(ParameterBlock block : parameterBlocks){
+				correlations.addAll(block.getCorrelations());				
+			}
+		}
+		return correlations;
+	}
+
+	/**
+	 * Creates first matrix row which will have only first omega as element.
+	 * 
+	 * @param eta
+	 * @param randomVar1
+	 */
+	private void createFirstMatrixRow(String eta, ParameterRandomVariableType randomVar1) {
+		if(etaToOmagaMap.get(randomVar1.getSymbId())== 1 && eta.equals(randomVar1.getSymbId())){
+			List<OmegaStatement> matrixRow = new ArrayList<OmegaStatement>();
+			String symbId = getNameFromParamRandomVariable(randomVar1);
+			matrixRow.add(getOmegaFromRandomVariable(symbId));
+			OmegaBlocks.put(randomVar1.getSymbId(), matrixRow);
+		}
+	}
+
+	/**
+	 * Get omega object from random variable provided.
+	 * This will set omega symb id as well as bounds if there are any.
+	 * 
+	 * @param randomVar
+	 * @return
+	 */
+	private OmegaStatement getOmegaFromRandomVariable(String omegaSymbId) {
+		OmegaStatement omegaStatement = null;
+		if(omegaSymbId!= null){
+			omegaStatement = new OmegaStatement(omegaSymbId);
+			omegaStatement.setParameterBounds(initialEstimates.get(omegaSymbId),lowerBounds.get(omegaSymbId),upperBounds.get(omegaSymbId));
+		}
+		return omegaStatement;
+	}
+	
+	/**
+	 * Initialise omega blocks maps and also update ordered eta to omegas from correlations map.
+	 * 
+	 * @param correlations
+	 */
+	public void initialiseOmegaBlocks(List<Correlation> correlations){
+		OmegaBlocks.clear();
+		//create correlations map
+		for(Correlation correlation : correlations){
+			etasToOmegasInCorrelation.put(correlation.rnd1.getSymbId(),getNameFromParamRandomVariable(correlation.rnd1));
+			etasToOmegasInCorrelation.put(correlation.rnd2.getSymbId(),getNameFromParamRandomVariable(correlation.rnd2));
+			etasToOmegasInCorrelation.put(correlation.correlationCoefficient.getSymbRef().getSymbIdRef(), 
+					correlation.correlationCoefficient.getSymbRef().getSymbIdRef());
+		}
+		
+		for(Iterator<Entry<String, Integer>> it = etaToOmagaMap.entrySet().iterator(); it.hasNext();) {
+		      Entry<String, Integer> entry = it.next();
+		      if(!etasToOmegasInCorrelation.keySet().contains(entry.getKey())){
+		    	  it.remove();
+		      }
+		}
+
+		for(String eta : etaToOmagaMap.keySet()){
+			ArrayList<OmegaStatement> statements = new ArrayList<OmegaStatement>();
+			for(int i=0;i<etaToOmagaMap.keySet().size();i++) statements.add(null);
+			OmegaBlocks.put(eta, statements);
 		}
 	}
 	
-	public String setOmegaBlock(){
-			
-			List<ParameterBlock> parameterBlocks = getScriptDefinition().getParameterBlocks();
-			List<Correlation> correlations = new ArrayList<Correlation>();
-
-			if(!parameterBlocks.isEmpty()){
-				for(ParameterBlock block : parameterBlocks){
-					correlations.addAll(block.getCorrelations());
-				}				
-			}
-			
-			return null;
-			
-	}
-	
+	/**
+	 * This method will set omega parameters apart from omega blocks.
+	 * 
+	 */
 	private void setOmegaParameters(){
 		List<ParameterBlock> parameterBlocks = getScriptDefinition().getParameterBlocks();
 		//Unless parameterBlocks is empty, getting first parameter Block.
-		List<Correlation> correlations = new ArrayList<Correlation>();
+		createOmegaBlocks();
 		if(!parameterBlocks.isEmpty()){
-			
-			for(ParameterBlock block : parameterBlocks){
-				correlations.addAll(block.getCorrelations());
-			}
 			for (ParameterRandomVariableType rv : parameterBlocks.get(0).getRandomVariables()) {
 				String symbId = getNameFromParamRandomVariable(rv);
-				if(symbId!= null){
-					OmegaStatement omegaStatement = new OmegaStatement(symbId);
-					omegaStatement.setParameterBounds(initialEstimates.get(symbId),lowerBounds.get(symbId),upperBounds.get(symbId));
-					OmegaStatements.put(symbId, omegaStatement);					
-				}
+					OmegaStatement omegaStatement = getOmegaFromRandomVariable(symbId);
+					if(omegaStatement!=null)
+						OmegaStatements.put(symbId, omegaStatement);					
 			}
 		}
 	}
 
-	private String getNameFromParamRandomVariable(ParameterRandomVariableType rv) {
+	/**
+	 * Returns parameter name from parameter random variable. 
+	 * The name can be obtained from either standard deviation or variance. 
+	 * @param rv
+	 * @return
+	 */
+	public String getNameFromParamRandomVariable(ParameterRandomVariableType rv) {
 		String symbId = null;
 		if (getDistributionTypeStdDev(rv) != null) {
 			symbId = getDistributionTypeStdDev(rv).getVar().getVarId();					
@@ -138,6 +296,11 @@ public class ParametersHelper {
 		return symbId;
 	}
 
+	/**
+	 * Get distribution type from standard deviation.
+	 * @param rv
+	 * @return
+	 */
 	public PositiveRealValueType getDistributionTypeStdDev(ParameterRandomVariableType rv){
 		final AbstractContinuousUnivariateDistributionType distributionType = rv.getAbstractContinuousUnivariateDistribution().getValue();
 		if (distributionType instanceof NormalDistribution) {
@@ -146,6 +309,11 @@ public class ParametersHelper {
 		return null;
 	}
 	
+	/**
+	 * Get distribution type from standard deviation.
+	 * @param rv
+	 * @return
+	 */
 	public PositiveRealValueType getDistributionTypeVariance(ParameterRandomVariableType rv){
 		final AbstractContinuousUnivariateDistributionType distributionType = rv.getAbstractContinuousUnivariateDistribution().getValue();
 		if (distributionType instanceof NormalDistribution) {
@@ -188,7 +356,9 @@ public class ParametersHelper {
 		}
 		return step;
 	}
-	
+
+	//Getters and setters
+
 	public ScriptDefinition getScriptDefinition() {
 		return scriptDefinition;
 	}
@@ -236,4 +406,25 @@ public class ParametersHelper {
 	public Map<String, SimpleParameterType> getSimpleParams() {
 		return simpleParams;
 	}
+	
+	public Map<String, Integer> setEtaToOmagaMap() {
+		return etaToOmagaMap;
+	}
+	
+	public Map<Integer, String> getOrderedEtasMap() {
+		return orderedEtasMap;
+	}
+
+	public void setEtaToOmagaMap(Map<String, Integer> etaToOmagaMap) {
+		this.etaToOmagaMap = etaToOmagaMap;
+	}
+	
+	public Map<String, List<OmegaStatement>> getOmegaBlocks() {
+		return OmegaBlocks;
+	}
+	
+	public String getOmegaBlockTitle() {
+		return omegaBlockTitle;
+	}
+
 }
