58c9b8b8cec0f68e4dc06a4ffc8dafa198726d0c
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2014-12-03 14:36:13 +0000

    adding testwarfarin.java again.!!

---
+++ src/test/java/eu/ddmore/converters/nonmem/TestWarfarin.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Cyprotex Discovery Ltd - All rights reserved.
+ ******************************************************************************/
+
+package eu.ddmore.converters.nonmem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import crx.converter.engine.ConversionDetail_;
+import eu.ddmore.convertertoolbox.api.response.ConversionDetail;
+import eu.ddmore.convertertoolbox.api.response.ConversionDetail.Severity;
+import eu.ddmore.convertertoolbox.api.response.ConversionReport;
+import eu.ddmore.convertertoolbox.api.response.ConversionReport.ConversionCode;
+
+public class TestWarfarin extends TestBase {
+	@Before
+	public void setUp() throws Exception {
+		inputXMLFile = "warfarin_PK_ODE/Warfarin-ODE-latest_Natallia.xml"; // Warfarin-ODE-latest_Natallia.xml Warfarin-ODE-latest.xml warfarin_PK_ODE_0.3.1.xml
+		init(inputXMLFile, V_0_3_1_SUBDIR);
+	}
+
+	@Test
+	public void test() {
+		assertTrue(dir.exists());
+		assertTrue(f.exists());
+		
+		ConversionReport report = c.performConvert(f, dir);
+		assertNotNull(report);
+		assertEquals(report.getReturnCode(), ConversionCode.SUCCESS);
+		List<ConversionDetail> details = report.getDetails(Severity.INFO);
+		assertFalse(details.isEmpty());
+		assertNotNull(details.get(0));
+		ConversionDetail_ detail = (ConversionDetail_) details.get(0);
+		f = detail.getFile();
+		assertTrue(f.exists());
+	}
+}
