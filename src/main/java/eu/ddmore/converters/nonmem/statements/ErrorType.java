ffde66cabe08ca12a0777cd1188480e9aecd4d7a
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2014-12-09 13:34:13 +0000

    DDMORE-932 Amended Error type specific details, added enums for constants

---
+++ src/main/java/eu/ddmore/converters/nonmem/statements/ErrorType.java
@@ -0,0 +1,43 @@
+package eu.ddmore.converters.nonmem.statements;
+
+import eu.ddmore.converters.nonmem.statements.ErrorStatement.ErrorConstant;
+import eu.ddmore.converters.nonmem.utils.Formatter;
+
+public enum ErrorType {
+	COMBINED_ERROR_1("combinedError1"){
+		@Override
+		String getStatement() {
+			return Formatter.endline("W = "+ErrorStatement.additive+
+					"+"+ErrorStatement.proportional+"*"+ErrorConstant.IPRED);
+		};
+	},
+	COMBINED_ERROR_2("combinedError2"){
+		@Override
+		String getStatement() {
+			return Formatter.endline("W = SQRT(("+ErrorStatement.additive+"*"+
+					ErrorStatement.additive+")"+"+ ("+ErrorStatement.proportional+"*"+
+					ErrorStatement.proportional+"*"+ErrorConstant.IPRED+"*"+ErrorConstant.IPRED+"))");
+		};
+	},
+	ADDITIVE_ERROR("additiveError"){
+		@Override
+		String getStatement() {
+			return Formatter.endline("W = "+ErrorStatement.additive+"+"+ErrorConstant.IPRED);
+		};
+	},
+	PROPORTIONAL_ERROR("proportionalError"){
+		@Override
+		String getStatement() {
+			return Formatter.endline("W = "+ErrorStatement.additive+"*"+ErrorConstant.IPRED);
+		};
+	};
+	
+	String error;
+	ErrorType(String error){
+		this.error = error;	
+	}
+	public String getErrorType() {
+		return this.error;
+	}
+	abstract String getStatement();
+}
