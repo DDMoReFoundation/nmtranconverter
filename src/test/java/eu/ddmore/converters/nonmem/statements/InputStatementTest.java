98102c8052bb8448a50d139051749fc88c35b576
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2015-08-06 11:24:34 +0100

    DDMORE-1401 Added more junit tests.

--- src/test/java/eu/ddmore/converters/nonmem/statements/InputStatementTest.java
+++ src/test/java/eu/ddmore/converters/nonmem/statements/InputStatementTest.java
@@ -3,7 +3,9 @@
  ******************************************************************************/
 package eu.ddmore.converters.nonmem.statements;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 import java.util.ArrayList;
@@ -26,13 +28,12 @@ import eu.ddmore.libpharmml.util.WrappedList;
 
 @RunWith(PowerMockRunner.class)
 public class InputStatementTest {
-    
+
     @Mock ConversionContext context;
     @Mock ExternalDataSet extDataSet;
     @Mock DataSet dataSet;
-    
+
     InputStatement inputStatement;
-    
     
     private static final String COL_ID_1 = ColumnConstant.ID.toString();
     private static final ColumnType COL_TYPE_1 = ColumnType.ID;
@@ -47,37 +48,70 @@ public class InputStatementTest {
     private static final ColumnDefinition TIME = new ColumnDefinition(COL_ID_2, COL_TYPE_2, COL_VALUE_2, COL_NUM_2);
     
     private static final String COL_ID_3 = "WT";
-    private static final ColumnType COL_TYPE_3 = ColumnType.COVARIATE;
+    private static final ColumnType COL_TYPE_3 = ColumnType.UNDEFINED;
     private static final SymbolType COL_VALUE_3 = SymbolType.ID;
     private static final Integer COL_NUM_3 = new Integer(3);
     private static final ColumnDefinition WT = new ColumnDefinition(COL_ID_3, COL_TYPE_3, COL_VALUE_3, COL_NUM_3);
     
-    private static final String COL_ID_4 = "EVID";
+    private static final String COL_ID_4 = "AMT";
     private static final ColumnType COL_TYPE_4 = ColumnType.COVARIATE;
     private static final SymbolType COL_VALUE_4 = SymbolType.ID;
     private static final Integer COL_NUM_4 = new Integer(4);
-    private static final ColumnDefinition EVID = new ColumnDefinition(COL_ID_4, COL_TYPE_4, COL_VALUE_4, COL_NUM_4);
+    private static final ColumnDefinition AMT = new ColumnDefinition(COL_ID_4, COL_TYPE_4, COL_VALUE_4, COL_NUM_4);
+    
+    private static final String COL_ID_5 = "EVID";
+    private static final ColumnType COL_TYPE_5 = ColumnType.UNDEFINED;
+    private static final SymbolType COL_VALUE_5 = SymbolType.ID;
+    private static final Integer COL_NUM_5 = new Integer(5);
+    private static final ColumnDefinition EVID = new ColumnDefinition(COL_ID_5, COL_TYPE_5, COL_VALUE_5, COL_NUM_5);
 
-    WrappedList<ColumnDefinition> dataColumns = new WrappedList<ColumnDefinition>();
+    WrappedList<ColumnDefinition> dataColumns;
     @Before
     public void setUp() throws Exception {
-
+        dataColumns = new WrappedList<ColumnDefinition>();
         dataColumns.add(ID);
         dataColumns.add(TIME);
-        dataColumns.add(WT);
         when(dataSet.getListOfColumnDefinition()).thenReturn(dataColumns);
-        
+
         when(extDataSet.getDataSet()).thenReturn(dataSet);
         List<ExternalDataSet> externalDataSets = new ArrayList<ExternalDataSet>();
         externalDataSets.add(extDataSet);
         when(context.retrieveExternalDataSets()).thenReturn(externalDataSets);
     }
-    
+
     @Test
     public void shouldReturnInputStatement() {
+        dataColumns.add(WT);
+        dataColumns.add(AMT);
+        dataColumns.add(EVID);
+        
         inputStatement = new InputStatement(context);
         String inputStmt = inputStatement.getStatement();
         assertNotNull("should return input statement",inputStmt);
-        
     }
+
+    @Test
+    public void shouldReturnInputStatementWithDrop() {
+        dataColumns.add(EVID);
+        
+        inputStatement = new InputStatement(context);
+        String inputStmt = inputStatement.getStatement();
+        assertNotNull("should return input statement",inputStmt);
+        assertTrue("should contain DROP for missing columns in order",inputStmt.contains("DROP"));
+    }
+    
+    @Test
+    public void shouldMarkColumnDropped() {
+        dataColumns.add(WT);
+        dataColumns.add(EVID);
+        
+        inputStatement = new InputStatement(context);
+        String inputStmt = inputStatement.getStatement();
+        assertNotNull("should return input statement",inputStmt);
+        //if column has columntype "UNDEFINED" then mark it as dropped
+        assertTrue("Should have column marked as dropped when columntype is undefined.",inputStmt.contains(WT.getColumnId()+"=DROP"));
+        //if column "EVID" has columntype "UNDEFINED" then do not mark it as dropped        
+        assertFalse("Column EVID should not be marked as dropped when columntype is undefined.",inputStmt.contains(EVID.getColumnId()+"=DROP"));
+    }
+    
 }
