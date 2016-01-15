d06a837b70518eeb3e7f2773d0e05d165953877a
Sachin Deshmukh <sdeshmukh@mango-solutions.com>
2016-01-15 11:08:58 +0000

    Sorted issue introduced around IOV unique value retrieval.

--- src/main/java/eu/ddmore/converters/nonmem/statements/InterOccVariabilityHandler.java
+++ src/main/java/eu/ddmore/converters/nonmem/statements/InterOccVariabilityHandler.java
@@ -109,6 +109,8 @@ public class InterOccVariabilityHandler {
         if(iovColumnUniqueValues.isEmpty() && columnWithOcc!=null){
             CsvReader reader = new CsvReader(dataFile.getAbsolutePath());
             try{
+                //Read the first record of data as column headers
+                reader.readHeaders();
                 while (reader.readRecord())
                 {
                     Double value = Double.parseDouble(reader.get(columnWithOcc.getColumnId()));
